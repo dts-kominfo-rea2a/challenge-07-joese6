ccc058e78a0bddc04607469d5ff7a0d6441d691b
Johan <joese6@gmail.com>
2022-06-12 20:48:42 +0700

    Tugas 11 - JS Promise - Tantangan

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (respon) => {
+  try {
+    const dataJumlahFilm = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
+    return dataJumlahFilm.filter(items => (items.hasil === respon)).length;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
